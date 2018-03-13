bf0fb57abbc6559789fc4c94b7a19674e7124c8f
JoseaScripts <32377523+JoseaScripts@users.noreply.github.com>
2018-03-13 14:30:07 -0300

    Create motion.test.sh
    Declaración de intenciones

---
+++ motion/motion.test.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# Generar un array con las opciones o un archivo de donde leo las opciones.
+# También se puede crear un archivo json (posiblemente la mejor opción si consigo averiguar bien como funciona de modo multidimensional)
